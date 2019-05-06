342b11bac6a3151bb76179aa161226cf3596d48b
vicious <vicious987@gmail.com>
2019-05-06 23:51:47 +0200

    array[2] to Point

--- main.cpp
+++ main.cpp
@@ -4,41 +4,45 @@
 #include <vector>
 #include <tuple>
 #include <cmath>
+#include <ctime>
 using namespace std;
 
 int n;
+struct Point 
+{
+	 int x, y;
+};
+bool cmp_x(const Point& a,const Point& b) { 
+    return a.x < b.x; 
+} 
 
-bool cmp_y(const int* u, const int* v){
-	return u[1] < v[1];
-}
-
-bool cmp_x(const int* u, const int* v){
-	return u[0] < v[0];
-}
-
-void printtab(int** t, int c){
+bool cmp_y(const Point& a,const Point& b) { 
+    return a.y < b.y; 
+} 
+void printpoints(Point* t,int c) {
 	printf("\n");
 	for(int i = 0; i < c; i++)
-		printf("(%d %d),", t[i][0], t[i][1]);
+		printf("(%d %d),", t[i].x , t[i].y);
 	printf("\n");
+	printf("points printed\n");
 }
 
-double sq_dst(int *a, int *b) {
-	int dx = b[0] - a[0];
-	int dy = b[1] - a[1];
+
+double sq_dst(Point &a, Point &b) {
+	int dx = b.x - a.x;
+	int dy = b.y - a.y;
 	return dx*dx + dy*dy;
 }
-
-double dst(int *a, int *b) {
+double dst(Point &a, Point &b) {
 	return sqrt(sq_dst(a, b));
 }
 
-bool in_range(int *p, double l, double dist){
-	double t = (double)p[0];
+bool in_range(Point &p, double l, double dist){
+	double t = (double)p.x;
 	return ((l - dist <= t) and (t <= l + dist));
 }
 
-inline tuple<int*, int*, int> closest3(int * p1, int *p2, int *p3){
+inline tuple<Point, Point, double> closest3(Point& p1, Point& p2, Point& p3){
 	auto d12 = sq_dst(p1, p2);
 	auto d23 = sq_dst(p2, p3);
 	auto d13 = sq_dst(p1, p3);
@@ -54,7 +58,7 @@ inline tuple<int*, int*, int> closest3(int * p1, int *p2, int *p3){
 			return make_tuple(p1, p3, sqrt(d13));
 }
 
-tuple<int*, int*, double> closest_pair(int ** tx, int ** ty, int ** typ, int start,int end){
+tuple<Point, Point, double> closest_pair(Point* tx, Point* ty, Point* typ, int start,int end){
 	//printf("call %d %d\n", start, end);
 	// base calls
 	if (end - start == 1){
@@ -67,7 +71,7 @@ tuple<int*, int*, double> closest_pair(int ** tx, int ** ty, int ** typ, int sta
 
 	// divide for L and R, create dividing lane
 	int middle = (start + end)/2;
-	double lane = (((start + end)%2 == 1) ? (((double)tx[middle][0] + tx[middle + 1][0]) / 2) : tx[middle][0]);
+	double lane = (((start + end)%2 == 1) ? (((double)tx[middle].x + tx[middle + 1].x) / 2) : tx[middle].x);
 	
 	//printf("sl:%d el:%d sr:%d sl:%d, lane:%f\n", start, middle, middle + 1, end, lane);
 	// recurrency 
@@ -78,7 +82,7 @@ tuple<int*, int*, double> closest_pair(int ** tx, int ** ty, int ** typ, int sta
 	
 	// choose smaller from l and r
 	double min_dist;
-	tuple<int*, int*, double> point_pair;
+	tuple<Point, Point, double> point_pair;
 	if (min_dist_l < min_dist_r) {
 		point_pair = point_pair_l;
 		min_dist = min_dist_l;	
@@ -93,8 +97,8 @@ tuple<int*, int*, double> closest_pair(int ** tx, int ** ty, int ** typ, int sta
 	int* point2;
 
 
-/*	
-	vector<int*> p_c;
+	
+	vector<Point> p_c;
 	p_c.reserve(n / 2);
 	for(int i = 0; i < n; i++)
 		if (in_range(ty[i], lane, min_dist))
@@ -113,7 +117,7 @@ tuple<int*, int*, double> closest_pair(int ** tx, int ** ty, int ** typ, int sta
 //	*/
 	
 	
-//*	
+/*	
 	int k = 0;
 	for (int i = 0; i < n; i++)
 		if (in_range(ty[i], lane, min_dist)){
@@ -129,46 +133,48 @@ tuple<int*, int*, double> closest_pair(int ** tx, int ** ty, int ** typ, int sta
 			}
 		}
 	}
-//*/
+*/
 	
 	return point_pair;
 }
 
-
 int main() {
+  	clock_t begin = clock();
 	//printf("start\n");
 	scanf("%d", &n);
-	int ** ax = new int*[n];
-	int ** ay = new int*[n];
-	int ** ay_prim = new int*[n];
-
+	Point * ax = new Point[n];
+	Point * ay = new Point[n];
+	Point * ay_prim = new Point[n];
 
 	int x, y;
 	for(int i = 0; i < n; i++){
 		scanf("%d %d", &x, &y);
-		ax[i] = new int[2];
-		ay[i] = new int[2];
-		ax[i][0] = x;
-		ax[i][1] = y;
-		ay[i][0] = x;
-		ay[i][1] = y;
+		ax[i].x = x;
+		ax[i].y = y;
+		ay[i].x = x;
+		ay[i].y = y;
 	}
 	sort(ax, ax + n, cmp_x);
 	sort(ay, ay + n, cmp_y);
-	//printf("sorted\n");
-	//printtab(ax, n);
-	//printtab(ay, n);
+
+	printpoints(ax, n);
+	printpoints(ay, n);
+
+	
 	//printf("calling\n");
+	
 	auto z = closest_pair(ax, ay, ay_prim, 0, n-1);
 	//printf("done1\n");
-	int* p1 = get<0>(z);
+	auto p1 = get<0>(z);
 	//printf("done2\n");
-	int* p2 = get<1>(z);
+	auto p2 = get<1>(z);
 	//printf("done3\n");
 	//auto d = get<2>(z);
-	printf("%d %d\n%d %d", p1[0], p1[1], p2[0], p2[1]); 
+	printf("%d %d\n%d %d", p1.x, p1.y, p2.x, p2.y); 
 	//printf("done4\n");
 	
+	
+	/*
 	for(int i = 0; i < n; ++i) {
 		delete[] ax[i];   
 		delete[] ay[i];   
@@ -177,7 +183,10 @@ int main() {
 	delete[] ax;
 	delete[] ay;
 	//delete[] ay_prim;   
-	
+	*/
+	clock_t end = clock();
+	double t = double(end - begin) / CLOCKS_PER_SEC;
+	printf("time :: %f\n", t);
 	return 0;
 }
 
